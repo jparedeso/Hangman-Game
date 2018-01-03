b62d56716ac3f9ff2eacf2206fd62c8751e0a333
jparedeso <jparedeso@gmail.com>
2018-01-03 10:29:19 -0500

    created basic arrays and objects for keys and countries.

---
+++ assets/javascript/game.js
@@ -0,0 +1,141 @@
+var userKey = [
+	"m", "n", "b", "v", "c", "x", "z", "a", "s", "d", "f", "g", "h", "j", "k", "l", "p", "o", "i", "u", "y", "t", "r", "e", "w", "q", " "
+];
+
+var countries = [
+	{
+		name: "Argentina",
+		continent: "South America"
+	},
+	
+	{
+		name: "Brazil",
+		continent: "South America"
+	},
+	
+	{
+		name: "Peru",
+		continent: "South America"
+	},
+
+	{
+		name: "Bolivia",
+		continent: "South America"
+	},
+
+	{
+		name: "Colombia",
+		continent: "South America"
+	},
+
+	{
+		name: "Panama",
+		continent: "Central America"
+	},
+
+	{
+		name: "Costa Rica",
+		continent: "Central America"
+	},
+
+	{
+		name: "Nicaragua",
+		continent: "Central America"
+	},
+
+	{
+		name: "El Salvador",
+		continent: "Central America"
+	},
+
+	{
+		name: "Guatemala",
+		continent: "Central America"
+	},
+
+	{
+		name: "United States of America",
+		continent: "North America"
+	},
+
+	{
+		name: "Canada",
+		continent: "North America"
+	},
+
+	{
+		name: "Ghana",
+		continent: "Africa"
+	},
+
+	{
+		name: "Botswana",
+		continent: "Africa"
+	},
+
+	{
+		name: "Rwanda",
+		continent: "Africa"
+	},
+
+	{
+		name: "Sierra Leone",
+		continent: "Africa"
+	},
+
+	{
+		name: "Zimbabwe",
+		continent: "Africa"
+	},
+
+	{
+		name: "Afghanistan",
+		continent: "Asia and Oceania"
+	},
+
+	{
+		name: "Japan",
+		continent: "Asia and Oceania"
+	},
+
+	{
+		name: "Thailand",
+		continent: "Asia and Oceania"
+	},
+
+	{
+		name: "Mongolia",
+		continent: "Asia and Oceania"
+	},
+
+	{
+		name: "India",
+		continent: "Asia and Oceania"
+	},
+
+	{
+		name: "Belgium",
+		continent: "Europe"
+	},
+
+	{
+		name: "Croatia",
+		continent: "Europe"
+	},
+
+	{
+		name: "Germany",
+		continent: "Europe"
+	},
+
+	{
+		name: "Herzegovina",
+		continent: "Europe"
+	},
+	{
+		name: "Switzerland",
+		continent: "Europe"
+	}
+];
+
+
